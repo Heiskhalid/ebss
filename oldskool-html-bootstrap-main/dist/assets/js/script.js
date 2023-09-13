99447a3f968c37ce2363483c1efd0a53f210d3fa
Heiskhalid <khaliddolapo@gmail.com>
2023-09-14 00:12:22 +0100

    holy spirit worked

--- oldskool-html-bootstrap-main/dist/assets/js/script.js
+++ oldskool-html-bootstrap-main/dist/assets/js/script.js
@@ -1,14 +1,22 @@
-
+<script src="https://player.vimeo.com/api/player.js"></script>
+<script>
   // Initialize Vimeo Players
-  var player1 = new Vimeo.Player('video1', { id: '1c4d78c517' });
-  var player2 = new Vimeo.Player('video2', { id: 'fc949ecb30' });
+  var player1 = new Vimeo.Player('video1', { id: '862981686' });
+  var player2 = new Vimeo.Player('video2', { id: '863368485' });
   // Add more players as needed
 
-  // Play the videos simultaneously
-  function playVideos() {
-    player1.play();
-    player2.play();
-    // Play more videos here if needed
-  }
-
+// Function to play all videos
+function playVideos() {
+  Promise.all([
+    player1.play(),
+    player2.play()
+    // Add more videos to play here if needed
+  ]).then(function() {
+    // All videos have started playing
+  }).catch(function(error) {
+    // Handle any errors
+    console.error('Error playing videos:', error);
+  });
+}
   window.addEventListener('load', playVideos);
+</script>
